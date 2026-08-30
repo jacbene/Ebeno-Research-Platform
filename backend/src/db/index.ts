19bf16b20cd38eb5950d6c6cde212a773edf9dfe
jacbene <jacquesbene301@gmail.com>
2026-08-30 21:05:25 +0100

    Suppression des fichiers inutilisés (Prisma, Drizzle, tests, etc.)

--- backend/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from 'drizzle-orm/sqlite3';
-import Database from 'sqlite3';
-
-const sqlite = new Database('dev.db');
-export const db = drizzle(sqlite);
-
-export * from './schema';
