040cd1173a7110eb781c16313421b21444833d26
Bene Mbama <jacquesbene301@gmail.com>
2026-08-30 18:45:25 +0100

    Comment out PrismaClient import in visualizationController
    Comment out the import statement for PrismaClient.

--- backend/src/controllers/visualizationController.ts
+++ backend/src/controllers/visualizationController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import visualizationService from '../services/visualizationService';
-import { PrismaClient } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
