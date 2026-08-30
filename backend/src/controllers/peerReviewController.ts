6d1f6e992937a06ae676dbc18f0b0a97d99f00f5
Bene Mbama <jacquesbene301@gmail.com>
2026-08-30 18:20:56 +0100

    Comment out prisma import in peerReviewController
    Commented out the import statement for prisma.

--- backend/src/controllers/peerReviewController.ts
+++ backend/src/controllers/peerReviewController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { prisma } from '../lib/prisma';
+//import { prisma } from '../lib/prisma';
 import { ReviewStatus, ReviewDecision } from '@prisma/client';
 
 export const createPeerReviewSubmission = async (req: Request, res: Response) => {
