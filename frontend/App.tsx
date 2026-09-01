59ec4a585d9454efc539022d81b8eb8404ee1294
jacbene <jacquesbene301@gmail.com>
2026-09-01 05:28:11 +0100

    Utilisation de l'instance api pour le login

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
